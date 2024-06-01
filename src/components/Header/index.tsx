7c386d364ffb44ca4cf76bbadd31e98fcb42b5ec
Pedro Germano <pedrogermano232@gmail.com>
2024-05-31 22:43:53 -0300

    feat: form data create

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,21 @@
+import { HeaderContainer } from "./styles";
+
+import logoIgnite from "../../assets/logo-ignite.svg";
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+
 export function Header() {
   return (
-    <>
-      <header>Header</header>
-    </>
+    <HeaderContainer>
+      <img src={logoIgnite} alt="logo ignite timer" />
+      <nav>
+        <NavLink title="Timer" to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink title="Home" to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
   );
 }
